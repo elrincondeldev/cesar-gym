4d3a938a45e72fb258364301c2dab014791453a5
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-08-08 16:03:49 +0200

    tesT

--- src/app/[slug]/route.ts
+++ src/app/[slug]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import UAParser from "ua-parser-js";
 
 const prisma = new PrismaClient();
 
@@ -9,6 +10,12 @@ export async function GET(
 ) {
   try {
     const { slug } = params;
+
+    // Parsear el User-Agent para detectar si es un móvil
+    const userAgent = request.headers.get("user-agent") || "";
+    const parser = new UAParser(userAgent);
+    const device = parser.getDevice();
+    const isMobile = device.type === "mobile" || device.type === "tablet";
 
     const link = await prisma.link.findFirst({
       where: { url: `https://cesar-gym.vercel.app/${slug}` },
@@ -30,28 +37,22 @@ export async function GET(
       data: { uses: link.uses - 1 },
     });
 
-    const response = NextResponse.redirect("https://wa.me/34601506486", 302);
+    if (isMobile) {
+      // Devolver JSON si es móvil
+      return NextResponse.json({ redirectTo: "https://wa.me/34601506486" });
+    } else {
+      // Redirección directa si es escritorio
+      const response = NextResponse.redirect("https://wa.me/34601506486", 302);
 
-    // Deshabilitar caché para esta respuesta
-    response.headers.set(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate"
-    );
-    response.headers.set("Pragma", "no-cache");
-    response.headers.set("Expires", "0");
+      response.headers.set(
+        "Cache-Control",
+        "no-store, no-cache, must-revalidate, proxy-revalidate"
+      );
+      response.headers.set("Pragma", "no-cache");
+      response.headers.set("Expires", "0");
 
-    // Asegurar compatibilidad con CORS
-    response.headers.set("Access-Control-Allow-Origin", "*");
-    response.headers.set(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    response.headers.set(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-
-    return response;
+      return response;
+    }
   } catch (error) {
     console.error("Error in GET request:", error);
     return NextResponse.json(
