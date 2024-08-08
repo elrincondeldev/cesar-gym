d564386342503586e3fa0f50395dbf4316743616
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-08-08 16:09:53 +0200

    final

--- src/app/[slug]/route.ts
+++ src/app/[slug]/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import UAParser from "ua-parser-js";
 
 const prisma = new PrismaClient();
 
@@ -9,23 +8,23 @@ export async function GET(
   { params }: { params: { slug: string } }
 ) {
   try {
-    const { slug } = params;
+    const p = "34601506486";
+    const m = "test";
+    console.log("test22");
 
-    // Parsear el User-Agent para detectar si es un móvil
-    const userAgent = request.headers.get("user-agent") || "";
-    const parser = new UAParser(userAgent);
-    const device = parser.getDevice();
-    const isMobile = device.type === "mobile" || device.type === "tablet";
+    const { slug } = params;
 
     const link = await prisma.link.findFirst({
       where: { url: `https://cesar-gym.vercel.app/${slug}` },
     });
 
     if (!link) {
+      console.log("Link not found");
       return NextResponse.redirect("/404", 302);
     }
 
     if (link.uses <= 0) {
+      console.log("No remaining uses");
       return NextResponse.json(
         { error: "No remaining uses for this link" },
         { status: 410 }
@@ -37,22 +36,23 @@ export async function GET(
       data: { uses: link.uses - 1 },
     });
 
-    if (isMobile) {
-      // Devolver JSON si es móvil
-      return NextResponse.json({ redirectTo: "https://wa.me/34601506486" });
-    } else {
-      // Redirección directa si es escritorio
-      const response = NextResponse.redirect("https://wa.me/34601506486", 302);
+    console.log("Redirecting to external URL");
+    const response = NextResponse.redirect(
+      `whatsapp://send?phone=${p}&text=${m}`,
+      302
+    );
 
-      response.headers.set(
-        "Cache-Control",
-        "no-store, no-cache, must-revalidate, proxy-revalidate"
-      );
-      response.headers.set("Pragma", "no-cache");
-      response.headers.set("Expires", "0");
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    response.headers.set(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
 
-      return response;
-    }
+    return response;
   } catch (error) {
     console.error("Error in GET request:", error);
     return NextResponse.json(
